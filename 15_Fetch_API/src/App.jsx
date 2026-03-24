6be2fa780304f9b184a70945b7bdfaa91d9c0658
Satyam1978 <satyam1978@users.noreply.github.com>
2026-03-24 19:27:34 +0530

    useEffect

--- 15_Fetch_API/src/App.jsx
+++ 15_Fetch_API/src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const App = () => {
-  
+  const [data,setdata] = useState([])
   const fetching = async () =>{
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  console.log(data)
+  const data =  await fetch('https://dummyjson.com/users');
+  const result = await data.json();
+  setdata(result.users)
+  console.log(result.users)
   }
   return (
     <div>
       <button onClick={fetching}>Click here</button>
-      
+      <div className="msg">
+        {data.map((e,idx) => {
+          return(
+            <p>{idx} {e.firstName} {e.lastName} </p>
+          )
+        })}
+      </div>
     </div>
   )
 }
